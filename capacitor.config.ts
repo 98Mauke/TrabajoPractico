fc466ad67de6665fea1e9556c578218208de6df3
Soy Mauro <mauroalex315@gmail.com>
2024-05-25 10:02:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NationFacts',
+  webDir: 'www'
+};
+
+export default config;
